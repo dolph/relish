7f28fd88dd82886ea85c6c6c90cded3eb01be052
Dolph Mathews <dolph.mathews@gmail.com>
2011-12-10 13:03:26 -0600

    Deep serialization works

--- relish/serializers/jsonify.py
+++ relish/serializers/jsonify.py
@@ -7,12 +7,14 @@ def serialize(obj):
     d = _wrap_dict(obj, d)
     return json.dumps(d)
 
+
 def deserialize(string, cls):
     """Deserialize a string, given the class"""
     d = json.loads(string)
     d = _unwrap_dict(d, cls)
     obj = _dict_to_obj(d, cls)
     return obj
+
 
 def _get_wrapper_name(cls):
     if hasattr(cls, '_relish'):
@@ -21,6 +23,7 @@ def _get_wrapper_name(cls):
         name = cls.__name__.lower()
 
     return name
+
 
 def _wrap_dict(obj, d):
     """Wrap a dictionary with the class name."""
@@ -31,6 +34,7 @@ def _wrap_dict(obj, d):
     else:
         return d
 
+
 def _unwrap_dict(d, cls):
     """Unwrap the class name from the dictionary"""
     name = _get_wrapper_name(cls)
@@ -40,6 +44,7 @@ def _unwrap_dict(d, cls):
     else:
         return d
 
+
 def _obj_to_dict(obj):
     """Walk an object and produce a corresponding dictionary.
 
@@ -47,11 +52,19 @@ def _obj_to_dict(obj):
     """
     d = dict()
     for attr in dir(obj):
-        if True and \
-                attr[0] != '_' and \
-                not callable(getattr(obj, attr)):
-            d[attr] = getattr(obj, attr)
+        # avoid private attributes
+        if attr[0] != '_':
+            value = getattr(obj, attr)
+            if hasattr(value, '_relish'):
+                if hasattr(value, '__class__'):
+                    # deep serialize
+                    dattr = _wrap_dict(value, _obj_to_dict(value))
+                    d.update(dattr)
+            elif not callable(value):
+                # normal attribute
+                d[attr] = value
     return d
+
 
 def _dict_to_obj(d, cls):
     """Populate an obj with values from a dictionary."""
